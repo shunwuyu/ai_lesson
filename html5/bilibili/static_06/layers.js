22a683003a0c0aca67a933226375ae62b61843a7
shunwuyu <shunwu2001@163.com>
2024-11-23 11:25:14 +0800

    163

---
+++ html5/bilibili/static_06/layers.js
@@ -0,0 +1,544 @@
+const layers = [
+  {
+      "resources": [
+          {
+              "src": "./static_06/01b79b5b5b38d7305a0ca1780c24662cc5a59f18.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.9
+      },
+      "rotate": {},
+      "translate": {
+          "offset": [
+              5,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 0,
+      "name": "18 bg"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/fe903820ca0f604ef24f16ff7ad51192ead3d0c8.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.9
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              0,
+              -10
+          ],
+          "offset": [
+              20,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 1,
+      "name": "17 远山"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/73fcb8a56776307147c140f68bf9c5793696ae88.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.9
+      },
+      "rotate": {},
+      "translate": {
+          "offset": [
+              40,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 2,
+      "name": "16 草中景"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/b8a5bcf77a07793ce50294a8cb14a9fb8f2cea83.png",
+              "id": 0
+          }
+      ],
+      "scale": {},
+      "rotate": {},
+      "translate": {
+          "initial": [
+              0,
+              -40
+          ],
+          "offset": [
+              30,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 5,
+      "name": "13a 藤蔓"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/1fd3d44b8f890b88cf52f6bcc041326b30e7cb62.png",
+              "id": 0
+          }
+      ],
+      "scale": {},
+      "rotate": {},
+      "translate": {
+          "initial": [
+              100,
+              0
+          ],
+          "offset": [
+              60,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 3,
+      "name": "15 松树中景"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/085bc5e7f70b625b62c1f5d374f55acbd828ab65.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.9
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              -140,
+              -50
+          ],
+          "offset": [
+              50,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 14,
+      "name": "03 顶部树枝"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/3e16264878dd88c1b8ead37aab07a9363186b18c.png",
+              "id": 0
+          }
+      ],
+      "scale": {},
+      "rotate": {},
+      "translate": {
+          "initial": [
+              0,
+              10
+          ],
+          "offset": [
+              70,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 4,
+      "name": "14 锅子"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/8902051ab71c8093a85ce2a71882054ee91ea631.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 1.2
+      },
+      "rotate": {
+          "offset": -40
+      },
+      "translate": {
+          "initial": [
+              230,
+              -40
+          ],
+          "offset": [
+              80,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 6,
+      "name": "12 悬挂猴"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/c9231e763120806f3e918d06ad3f415e920c2b73.png",
+              "id": 0
+          }
+      ],
+      "scale": {},
+      "rotate": {},
+      "translate": {
+          "initial": [
+              660,
+              0
+          ],
+          "offset": [
+              80,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 9,
+      "name": "09 右边大树"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/67a3c27832d63060d88fbd7bdb2fc543e3fb9ddf.png",
+              "id": 0
+          }
+      ],
+      "scale": {},
+      "rotate": {},
+      "translate": {
+          "initial": [
+              750,
+              0
+          ],
+          "offset": [
+              100,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 10,
+      "name": "08 右边雪地"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/c651a0183b81857191c86b133c5a777059b13571.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.95
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              530,
+              15
+          ],
+          "offset": [
+              120,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 7,
+      "name": "11 右边三猴"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/6afc90f36610318b4785a52c1a51ef6f1de9d25b.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.9
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              560,
+              15
+          ],
+          "offset": [
+              270,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 8,
+      "name": "10 右边大猴"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/476679726643b3b6cc5599b882d85953ef34f3d9.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.9
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              -190,
+              -5
+          ],
+          "offset": [
+              100,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 11,
+      "name": "07 33"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/121335f79c78bb6f47e4fc7dae4b0af0eac804bb.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.9
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              -600,
+              0
+          ],
+          "offset": [
+              180,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 15,
+      "name": "02_左边树干"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/590a6dd64ff00d5d0e63e230188939c405253452.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.95
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              -350,
+              25
+          ],
+          "offset": [
+              150,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 12,
+      "name": "06 左边两只猴"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/e00c60ae700774655af2e2516a13a5b1fa2fcfe7.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.95
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              -580,
+              25
+          ],
+          "offset": [
+              251,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 13,
+      "name": "05 左边一只猴"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/357dd346995f678c43eb6bf51fe02f47bbe7db7d.png",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 0.8
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              -1100,
+              20
+          ],
+          "offset": [
+              360,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 16,
+      "name": "01 前景树枝"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/bae427c01d895b63e6aaf6b0c239b86027527e6a.png",
+              "id": 0
+          }
+      ],
+      "scale": {},
+      "rotate": {},
+      "translate": {
+          "initial": [
+              20,
+              10
+          ],
+          "offset": [
+              80,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "initial": 0.3,
+          "wrap": "clamp"
+      },
+      "id": 17,
+      "name": "00 光"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/3b226df45a1d6af2cdfeb20b9963849e13356f3d.webm",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 1.1
+      },
+      "rotate": {},
+      "translate": {
+          "offset": [
+              30,
+              0
+          ]
+      },
+      "blur": {},
+      "opacity": {
+          "wrap": "clamp"
+      },
+      "id": 18,
+      "name": "00 雪"
+  },
+  {
+      "resources": [
+          {
+              "src": "./static_06/dead007d170ae4ae5a69ef6ee044e0ede574627e.webm",
+              "id": 0
+          }
+      ],
+      "scale": {
+          "initial": 2.1
+      },
+      "rotate": {},
+      "translate": {
+          "initial": [
+              20,
+              -40
+          ],
+          "offset": [
+              36,
+              0
+          ]
+      },
+      "blur": {
+          "initial": 5
+      },
+      "opacity": {
+          "initial": 0.3,
+          "wrap": "clamp"
+      },
+      "id": 19,
+      "name": "00 烟雾"
+  }
+]
