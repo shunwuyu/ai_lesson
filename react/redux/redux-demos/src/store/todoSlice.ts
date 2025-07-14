41d53cfd2d5c406c699da87951fb865470a7d25a
shunwuyu <shunwu2001@163.com>
2025-07-14 17:40:47 +0800

    redux

--- react/redux/redux-demos/src/store/todoSlice.ts
+++ react/redux/redux-demos/src/store/todoSlice.ts
@@ -1,8 +1,12 @@
 // src/todoSlice.ts
 // PayloadAction 是 Redux Toolkit 中的一个类型，用于定义 Redux action 的 payload 类型。
 // PayloadAction 是 Redux Toolkit 提供的一个类型工具，用于明确 Redux action 中 payload 的数据类型。
+// @reduxjs/toolkit 简化了 Redux 开发流程，
+// createSlice 是 Redux Toolkit 提供的一个函数，用来简化 Redux 中 reducer 和 action 的创建。
+// 公司
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
+// reducer是纯函数 
+// 接收当前状态和action，返回更新后的状态。
 export interface Todo {
   id: number;
   text: string;
@@ -28,6 +32,8 @@ export const todoSlice = createSlice({
         text: action.payload,
         completed: false,
       };
+      // slice 内置immer 
+      // 状态是只读的，并且只能通过纯函数（reducers）来更新
       state.todos.push(newTodo);
     },
     toggleTodo: (state, action: PayloadAction<number>) => {
@@ -41,7 +47,8 @@ export const todoSlice = createSlice({
     },
   },
 });
-
+// action 返回
+// Redux Toolkit 会为每个 reducer 自动生成一个对应的 action 创建函数：
 export const { addTodo, toggleTodo, deleteTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
