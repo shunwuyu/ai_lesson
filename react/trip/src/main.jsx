8c8f7d7810903117198ff68bc3bbe11745429d33
shunwuyu <shunwu2001@163.com>
2025-07-24 21:29:44 +0800

    路由配置和tabbar组件

--- react/trip/src/main.jsx
+++ react/trip/src/main.jsx
@@ -1,12 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'lib-flexible' // 移动端适配
 import './index.css'
+import {
+  BrowserRouter as Router
+} from 'react-router-dom'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Router>
     <App />
-  </StrictMode>,
+  </Router>,
 )
