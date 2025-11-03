e2943a0a19a97d6911bfc59c4901e419b40c9e5d
shunwuyu <shunwu2001@163.com>
2025-11-03 17:40:51 +0800

    hujiang

---
+++ weapp/hujiang/miniprogram-2/pages/index/index.js
@@ -0,0 +1,95 @@
+Page({
+  data: {
+    navigation: [
+      {
+        typeName:'有声读物',
+        typePic: 'https://m.360buyimg.com/babel/jfs/t20270715/237082/37/21845/7616/6694edddFc764124a/38d00b686257b0f4.png'
+      },
+      {
+        typeName:'名文赏析',
+        typePic: 'https://p3-passport.byteacctimg.com/img/user-avatar/91e8c76c79e01ae6c8badbdedf31063c~120x120.awebp'
+      },
+      {
+        typeName:'热门词汇',
+        typePic: 'https://p3-passport.byteacctimg.com/img/user-avatar/91e8c76c79e01ae6c8badbdedf31063c~120x120.awebp'
+      },
+      {
+        typeName:'新鲜事',
+        typePic: 'https://p3-passport.byteacctimg.com/img/user-avatar/91e8c76c79e01ae6c8badbdedf31063c~120x120.awebp'
+      },
+      {
+        typeName:'四六级',
+        typePic: 'https://p3-passport.byteacctimg.com/img/user-avatar/91e8c76c79e01ae6c8badbdedf31063c~120x120.awebp'
+      },
+      {
+        typeName:'娱乐动态',
+        typePic: 'https://p3-passport.byteacctimg.com/img/user-avatar/91e8c76c79e01ae6c8badbdedf31063c~120x120.awebp'
+      },
+      {
+        typeName:'口语对话',
+        typePic: 'https://p3-passport.byteacctimg.com/img/user-avatar/91e8c76c79e01ae6c8badbdedf31063c~120x120.awebp'
+      },
+      {
+        typeName:'留学雅思',
+        typePic: 'https://p3-passport.byteacctimg.com/img/user-avatar/91e8c76c79e01ae6c8badbdedf31063c~120x120.awebp'
+      },
+    ],
+    advertPic: [
+      'https://img.huxiucdn.com/article/cover/202403/08/120109044175.jpg?imageView2/1/w/686/h/422/|imageMogr2/strip/interlace/1/quality/85/format/webp',
+      'https://img.huxiucdn.com/article/cover/202511/03/154301762445.jpg?imageView2/1/w/1372/h/720/|imageMogr2/strip/interlace/1/quality/85/format/webp'
+    ],
+    articles: [
+      {
+        id: 1,
+        title:'Cursor初体验',
+        time:'3分钟前',
+        imgUrl:'https://p9-xtjj-sign.byteimg.com/tos-cn-i-73owjymdk6/f7949c47721a40a7afaaa44b4a2e2622~tplv-73owjymdk6-jj-mark-v1:0:0:0:0:5o6Y6YeR5oqA5pyv56S-5Yy6IEAg56iL5bqP5ZGY5rW35Yab:q75.awebp?rk3s=f64ab15b&x-expires=1762766342&x-signature=Px7w5IyqXWOjc7PYBb48YSbPPGA%3D',
+        text:'cursor好耶，正是好，cursor棒耶，棒棒鸡哦。你有trae,我有cursor。',
+        listTitle:'listTitle',
+        listDesc:'listDesc'
+      },
+      {
+        id: 2,
+        title:'Cursor初体验',
+        time:'3分钟前',
+        imgUrl:'https://p9-xtjj-sign.byteimg.com/tos-cn-i-73owjymdk6/f7949c47721a40a7afaaa44b4a2e2622~tplv-73owjymdk6-jj-mark-v1:0:0:0:0:5o6Y6YeR5oqA5pyv56S-5Yy6IEAg56iL5bqP5ZGY5rW35Yab:q75.awebp?rk3s=f64ab15b&x-expires=1762766342&x-signature=Px7w5IyqXWOjc7PYBb48YSbPPGA%3D',
+        text:'cursor好耶，正是好，cursor棒耶，棒棒鸡哦。你有trae,我有cursor。'
+      },
+      {
+        id: 3,
+        title:'Cursor初体验',
+        time:'3分钟前',
+        imgUrl:'https://p9-xtjj-sign.byteimg.com/tos-cn-i-73owjymdk6/f7949c47721a40a7afaaa44b4a2e2622~tplv-73owjymdk6-jj-mark-v1:0:0:0:0:5o6Y6YeR5oqA5pyv56S-5Yy6IEAg56iL5bqP5ZGY5rW35Yab:q75.awebp?rk3s=f64ab15b&x-expires=1762766342&x-signature=Px7w5IyqXWOjc7PYBb48YSbPPGA%3D',
+        text:'cursor好耶，正是好，cursor棒耶，棒棒鸡哦。你有trae,我有cursor。'
+      },
+      {
+        id: 4,
+        title:'Cursor初体验',
+        time:'3分钟前',
+        imgUrl:'https://p9-xtjj-sign.byteimg.com/tos-cn-i-73owjymdk6/f7949c47721a40a7afaaa44b4a2e2622~tplv-73owjymdk6-jj-mark-v1:0:0:0:0:5o6Y6YeR5oqA5pyv56S-5Yy6IEAg56iL5bqP5ZGY5rW35Yab:q75.awebp?rk3s=f64ab15b&x-expires=1762766342&x-signature=Px7w5IyqXWOjc7PYBb48YSbPPGA%3D',
+        text:'cursor好耶，正是好，cursor棒耶，棒棒鸡哦。你有trae,我有cursor。'
+      },
+      {
+        id: 5,
+        title:'Cursor初体验',
+        time:'3分钟前',
+        imgUrl:'https://p9-xtjj-sign.byteimg.com/tos-cn-i-73owjymdk6/f7949c47721a40a7afaaa44b4a2e2622~tplv-73owjymdk6-jj-mark-v1:0:0:0:0:5o6Y6YeR5oqA5pyv56S-5Yy6IEAg56iL5bqP5ZGY5rW35Yab:q75.awebp?rk3s=f64ab15b&x-expires=1762766342&x-signature=Px7w5IyqXWOjc7PYBb48YSbPPGA%3D',
+        text:'cursor好耶，正是好，cursor棒耶，棒棒鸡哦。你有trae,我有cursor。'
+      },
+      {
+        id: 6,
+        title:'Cursor初体验',
+        time:'3分钟前',
+        imgUrl:'https://p9-xtjj-sign.byteimg.com/tos-cn-i-73owjymdk6/f7949c47721a40a7afaaa44b4a2e2622~tplv-73owjymdk6-jj-mark-v1:0:0:0:0:5o6Y6YeR5oqA5pyv56S-5Yy6IEAg56iL5bqP5ZGY5rW35Yab:q75.awebp?rk3s=f64ab15b&x-expires=1762766342&x-signature=Px7w5IyqXWOjc7PYBb48YSbPPGA%3D',
+        text:'cursor好耶，正是好，cursor棒耶，棒棒鸡哦。你有trae,我有cursor。'
+      },
+      {
+        id: 7,
+        title:'Cursor初体验',
+        time:'3分钟前',
+        imgUrl:'https://p9-xtjj-sign.byteimg.com/tos-cn-i-73owjymdk6/f7949c47721a40a7afaaa44b4a2e2622~tplv-73owjymdk6-jj-mark-v1:0:0:0:0:5o6Y6YeR5oqA5pyv56S-5Yy6IEAg56iL5bqP5ZGY5rW35Yab:q75.awebp?rk3s=f64ab15b&x-expires=1762766342&x-signature=Px7w5IyqXWOjc7PYBb48YSbPPGA%3D',
+        text:'cursor好耶，正是好，cursor棒耶，棒棒鸡哦。你有trae,我有cursor。'
+      }
+    ]
+  }
+})
---
+++ weapp/hujiang/readme.md
@@ -0,0 +1,13 @@
+https://juejin.cn/post/6844903860071301127?searchId=20251103164340DDC9CF57237BDDA7A700
+
+- 安装vant
+  npm i @vant/weapp -S --production
+  - 构建npm 
+  - index.json
+  ```
+  "usingComponents": {
+    "van-search": "@vant/weapp/search/index"
+  }
+  <vant-search />
+  ```
+- 
