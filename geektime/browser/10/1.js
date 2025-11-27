826087e506f7b1b8d88458dcacac8bc6bba2d022
shunwuyu <shunwu2001@163.com>
2025-11-27 10:24:15 +0800

    closure

---
+++ geektime/browser/10/1.js
@@ -0,0 +1,9 @@
+function bar() {
+    console.log(myName) // 极客时间
+}
+function foo() {
+    var myName = "极客邦"
+    bar()
+}
+var myName = "极客时间"
+foo()
---
+++ geektime/browser/10/4.js
@@ -0,0 +1,14 @@
+var bar = {
+    myName:"time.geekbang.com",
+    printName: function () {
+        console.log(myName)
+    }    
+}
+function foo() {
+    let myName = "极客时间"
+    return bar.printName
+}
+let myName = "极客邦"
+let _printName = foo()
+_printName() // 极客邦
+bar.printName() // 极客邦
