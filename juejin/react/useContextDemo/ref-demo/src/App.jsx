7fb017fbf024871f4d83e8904d597dd84717a4e8
shunwuyu <shunwu2001@163.com>
2025-12-27 10:05:08 +0800

    context

--- juejin/react/useContextDemo/ref-demo/src/App.jsx
+++ juejin/react/useContextDemo/ref-demo/src/App.jsx
@@ -1,9 +1,10 @@
-import { useRef } from 'react'  
+import { useRef, useState } from 'react'  
 // useRef 提供一个在组件整个生命周期内都稳定存在的可变容器，
 // 修改它不会触发重新渲染
 function App() {
+  const [count, setCount] = useState(0);
   const timerRef = useRef(null)
-
+  console.log('---------------------');
   const start = () => {
     timerRef.current = setInterval(() => {
       console.log('tick')
@@ -18,6 +19,8 @@ function App() {
     <>
       <button onClick={start}>开始</button>
       <button onClick={stop}>停止</button>
+      <div>当前计数: {count}</div>
+      <button onClick={() => setCount(count + 1)}>增加</button>
     </>
   )
 }
