ba004363d06fe034dc150f65df6d61dbd1ccff01
shunwuyu <shunwu2001@163.com>
2026-02-03 12:11:43 +0800

    search

--- project/ai_fullstack/nest-prisma-demo/src/search/search.service.ts
+++ project/ai_fullstack/nest-prisma-demo/src/search/search.service.ts
@@ -4,10 +4,27 @@ import {
 import {
   OpenAIEmbeddings
 } from '@langchain/openai'
+import * as fs from 'fs/promises';
+import path from 'path';
+
+interface Post {
+  title: string;
+  category: string;
+  embedding: number[];
+}
+
+export function cosineSimilarity(v1: number[], v2: number[]): number {
+    const dotProduct = v1.reduce((sum, val, i) => sum + val * v2[i], 0);
+    const normV1 = Math.sqrt(v1.reduce((sum, val) => sum + val * val, 0));
+    const normV2 = Math.sqrt(v2.reduce((sum, val) => sum + val * val, 0));
+    return dotProduct / (normV1 * normV2);
+}
+
 
 @Injectable()
 export class SearchService {
   private embeddings: OpenAIEmbeddings;
+  private posts: Post[] = [];
   constructor(
   ) {
     this.embeddings = new OpenAIEmbeddings({
@@ -17,12 +34,42 @@ export class SearchService {
       },
       model: 'text-embedding-ada-002'
     });
+    this.loadPosts();
   }
-  async search(keyword: string) {
+  private async loadPosts() {
+    // console.log(__dirname, "/////")
+    try {
+      const filePath = path.join(__dirname, '../../', 'data', 'posts_with_embedding.json');
+      const data = await fs.readFile(filePath, 'utf-8');
+      // console.log(data, "////////");
+      this.posts = JSON.parse(data);
+    } catch (error) {
+      console.error('Failed to load posts:', error);
+      this.posts = [];
+    }
+  }
+
+  async search(keyword: string, topK = 3) {
     console.log(keyword)
     // const embeddings = this.embeddings.embedQuery(keyword);
     const vector = await this.embeddings.embedQuery(keyword);
-    console.log(vector)
-    return { keyword };
+    console.log(vector);
+    // console.log(cosineSimilarity(vector, this.posts[0].embedding));
+    // return 
+    // 2. 计算每个帖子的相似度
+    const results = this.posts
+      .map((post) => ({
+        ...post,
+        similarity: cosineSimilarity(vector, post.embedding),
+      }))
+      .sort((a, b) => b.similarity - a.similarity) // 降序：越相似越靠前
+      .slice(0, topK);
+    console.log(results, "------");
+    const data = results.map((item) => item.title);
+    // 3. 返回结构化结果（可根据前端需要调整）
+    return {
+      code: 0,
+      data
+    }
   }
 }
